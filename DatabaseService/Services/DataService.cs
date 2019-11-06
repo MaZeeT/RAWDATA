c5ccb3926b54ec5f1622c4bd39e6fe6a0ed123c4
Pornflake <satanika9@gmail.com>
2019-11-06 06:59:12 +0100

    build string from searchtokens
    build searchstring + comments

--- DatabaseService/Services/DataService.cs
+++ DatabaseService/Services/DataService.cs
@@ -23,16 +23,23 @@ namespace DatabaseService
 
         public IList<Search> Search(string searchstring, int? searchtypecode)
         {
-
+            ////// for performing searches with appsearch on the db
+            ///
             // AuthUser()
             // if ok AddSearchHistory()
             // do actual search using appsearch in db and build results
+
+            //need db context and searchtype lookuptable
             using var db = new StackoverflowContext();
             Modules.SearchTypeLookupTable st = new Modules.SearchTypeLookupTable();
-            var search = new NpgsqlParameter("search", NpgsqlTypes.NpgsqlDbType.Text);
-            search.Value = searchstring;
-            //search.Value = "chocolate fudge";
 
+            ////get params for db.func
+            ///
+            //build searchstring
+            var search = new NpgsqlParameter("search", NpgsqlTypes.NpgsqlDbType.Text);
+            search.Value = BuildSearchString(searchstring);
+
+            //lookup searchtype string
             var searchtype = new NpgsqlParameter("searchtype", NpgsqlTypes.NpgsqlDbType.Text);
             if (searchtypecode >= 0 && searchtypecode <= 3)
             {
@@ -40,27 +47,47 @@ namespace DatabaseService
             }
             else searchtype.Value = st.searchType[3];
 
+            //userid is hardcoded for now; should be returned from auth or sth
             var appuserid = new NpgsqlParameter("appuserid", NpgsqlTypes.NpgsqlDbType.Integer);
             appuserid.Value = 2;
 
+            //call db.func appsearch
             return db.Search
                 .FromSqlRaw("SELECT * from appsearch(@appuserid, @searchtype, @search) limit 10", appuserid, searchtype, search)
                 .ToList();
         }
 
+        private static string BuildSearchString(string searchstring)
+        {
+            string[] separators = { ",", ".", "..." };
+
+            string[] words = searchstring.Split(separators, System.StringSplitOptions.RemoveEmptyEntries);
+            System.Console.WriteLine($"{words.Length} tokens in search");
+
+            string finalstring = string.Join(" ", words);
+            System.Console.WriteLine("Built search string: " + finalstring);
+            return finalstring;
+        }
+
         public IList<WordRank> WordRank(string searchstring, int? searchtypecode)
         {
-
+            ////// for performing searches with wordrank on the db
+            ///
             // AuthUser()
             // if ok AddSearchHistory()
             // do actual search using appsearch in db and build results
+
+            //need db context and searchtype lookuptable
             using var db = new StackoverflowContext();
             Modules.SearchTypeLookupTable st = new Modules.SearchTypeLookupTable();
 
+            ////get params for db.func
+            ///
+            //build searchstring
             var search = new NpgsqlParameter("search", NpgsqlTypes.NpgsqlDbType.Text);
-            search.Value = searchstring;
-            //search.Value = "chocolate fudge";
+            search.Value = BuildSearchString(searchstring);
 
+            //lookup searchtype string
             var searchtype = new NpgsqlParameter("searchtype", NpgsqlTypes.NpgsqlDbType.Text);
             if (searchtypecode >= 4 && searchtypecode <= 5)
             {
@@ -68,9 +95,11 @@ namespace DatabaseService
             }
             else searchtype.Value = st.searchType[5];
 
+            //userid is hardcoded for now; should be returned from auth or sth
             var appuserid = new NpgsqlParameter("appuserid", NpgsqlTypes.NpgsqlDbType.Integer);
             appuserid.Value = 2;
 
+            //call db.func wordrank
             return db.WordRank
                 .FromSqlRaw("SELECT * from wordrank(@appuserid, @searchtype, @search) limit 10", appuserid, searchtype, search)
                 .ToList();
