df95eb8db43722ec365bcecac8ea0350c12b91e9
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-11-07 20:24:45 +0100

    refactoring names

---
+++ DatabaseService/Services/AppUsersHandler.cs
@@ -0,0 +1,17 @@
+using DatabaseService.Modules;
+
+namespace DatabaseService.Services
+{
+    public class AppUsersHandler : IAppUsersService
+    {
+        public AppUser GetAppUser(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AppUser GetAppUser(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ DatabaseService/Services/IAppUsersService.cs
@@ -0,0 +1,15 @@
+using DatabaseService.Modules;
+
+namespace DatabaseService.Services
+{
+    public interface IAppUsersService
+    {
+        //todo suggestions to the interface
+        // bool CreateAppUser(string name);
+        // bool UpdateAppUser();
+        // bool DeleteAppUser(int id);
+        // bool DeleteAppUser(string name);
+        AppUser GetAppUser(int id);
+        AppUser GetAppUser(string name);
+    }
+}
