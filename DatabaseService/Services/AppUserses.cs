0ca2f0d0d918790daa943e9bf877d31e9eb125e7
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-11-07 20:15:56 +0100

    renamed

---
+++ DatabaseService/Services/AppUserses.cs
@@ -0,0 +1,35 @@
+namespace DatabaseService.Services
+{
+    public class AppUserses : IAppUsers
+    {
+        public bool CreateAppUser(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpdateAppUser()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DeleteAppUser(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DeleteAppUser(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AppUserses GetAppUser(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AppUserses GetAppUser(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
