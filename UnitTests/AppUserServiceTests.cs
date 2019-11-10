9277ef9ead6f60d139f14e4c52b83730e701c172
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-11-10 14:39:20 +0100

    added blank tests for appUser

---
+++ UnitTests/AppUserServiceTests.cs
@@ -0,0 +1,44 @@
+using Xunit;
+
+namespace UnitTests
+{
+    public class AppUserServiceTests
+    {
+        [Fact]
+        private void DatabaseModify()
+        {
+        }
+
+        [Fact]
+        public void GetAppUser()
+        {
+        }
+
+        [Fact]
+        public void GetAppUserId()
+        {
+        }
+
+        [Fact]
+        public void CreateAppUser()
+        {
+        }
+
+        [Fact]
+        public void UpdateAppUserName()
+        {
+        }
+
+        [Fact]
+        public void DeleteAppUser()
+        {
+        }
+
+        [Fact]
+        public void AppUserExist()
+        {
+        }
+
+
+    }
+}
