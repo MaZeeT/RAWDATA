b6a4e2df1f936439e57253d3ee3e20786d227e61
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-11-11 16:37:33 +0100

    added classes

---
+++ DatabaseService/Services/IHistoryService.cs
@@ -0,0 +1,16 @@
+﻿using DatabaseService.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DatabaseService.Services
+{
+    public interface IHistoryService
+    {
+        bool Add(History history);
+        History Get(int historyId);
+        bool Delete(int historyId);
+        bool HistoryExist(int id);
+    }
+}
