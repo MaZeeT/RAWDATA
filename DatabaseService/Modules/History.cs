8dd526d9afbb385fa43811ba25c1a9e06f10a511
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-11-11 16:38:23 +0100

    added file

---
+++ DatabaseService/Modules/History.cs
@@ -0,0 +1,13 @@
+namespace DatabaseService.Modules
+{
+    public class History
+    {
+        private int Id;
+        private int Userid;
+        private int Postid;
+        private string PostsTableName;
+        private string Date;
+        private bool isBookmark;
+        
+    }
+}
