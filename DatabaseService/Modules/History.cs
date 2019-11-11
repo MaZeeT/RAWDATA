bc110d38eec51547bd0b185cc2316edba5f0d0ee
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-11-11 17:57:59 +0100

    Fixed History mapping

--- DatabaseService/Modules/History.cs
+++ DatabaseService/Modules/History.cs
@@ -2,12 +2,12 @@ namespace DatabaseService.Modules
 {
     public class History
     {
-        public int Id;
-        public int Userid;
-        public int Postid;
-        public string PostsTableName;
-        public string Date;
-        public bool isBookmark;
+        public int Id { get; }
+        public int Userid { set; get; }
+        public int Postid { set; get; }
+        public string PostsTableName { set; get; }
+        public string Date { set; get; }
+        public bool isBookmark { set; get; }
         
     }
 }
