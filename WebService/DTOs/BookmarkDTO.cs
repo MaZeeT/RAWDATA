776dd84d181a18fb2964725d7fdb14a6ffe6da42
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-11-15 22:29:27 +0100

    Create BookmarkDTO.cs

---
+++ WebService/DTOs/BookmarkDTO.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebService.DTOs
+{
+    public class BookmarkDTO
+    {
+        public string Title { get; set; }
+        public string ThreadUrl{ get; set; }
+        public DateTime Date{ get; set; }
+    }
+}
