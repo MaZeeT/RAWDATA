ed94133f3af187672fd9b57985085bf6490b560d
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-11-15 22:31:16 +0100

    formate

--- WebService/DTOs/BookmarkDTO.cs
+++ WebService/DTOs/BookmarkDTO.cs
@@ -5,7 +5,7 @@ namespace WebService.DTOs
     public class BookmarkDTO
     {
         public string Title { get; set; }
-        public string ThreadUrl{ get; set; }
-        public DateTime Date{ get; set; }
+        public string ThreadUrl { get; set; }
+        public DateTime Date { get; set; }
     }
 }
--- WebService/DTOs/HistoryDTO.cs
+++ WebService/DTOs/HistoryDTO.cs
@@ -4,8 +4,8 @@ namespace WebService.DTOs
 {
     public class HistoryDTO
     {
-        public string Title{ get; set; }
-        public string ThreadUrl{ get; set; }
-        public DateTime Date{ get; set; }
+        public string Title { get; set; }
+        public string ThreadUrl { get; set; }
+        public DateTime Date { get; set; }
     }
 }
