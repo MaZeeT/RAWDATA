14ca3ed0f0f5193a71c3c571180801b08c36e901
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-11-15 22:29:21 +0100

    Create HistoryDTO.cs

---
+++ WebService/DTOs/HistoryDTO.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebService.DTOs
+{
+    public class HistoryDTO
+    {
+        public string Title{ get; set; }
+        public string ThreadUrl{ get; set; }
+        public DateTime Date{ get; set; }
+    }
+}
