53271c7785884bc0bd04a81be48759b1bd9ac5b5
Monica Toader <toadermonica@gmail.com>
2019-11-17 01:53:04 +0100

    final annot wsl tests and auth

--- UnitTests/WebServiceUnitTests/AnnotationsTests.cs
+++ UnitTests/WebServiceUnitTests/AnnotationsTests.cs
@@ -39,8 +39,8 @@ namespace UnitTests.WebServiceUnitTests
         {
             var user = new LoginUserDto
             {
-                Username = "Fun",
-                Password = "Birds"
+                Username = "Monica",
+                Password = "Toader"
             };
 
             var (loggedInUser, statusCode) = PostData(AuthenticateUserUrl+"/tokens", user, string.Empty);
@@ -71,9 +71,89 @@ namespace UnitTests.WebServiceUnitTests
             var annotationUrl = (string)annotation["url"];
 
             Assert.Equal(HttpStatusCode.OK, statusCode);
-            Assert.NotNull(annotationUrl);
+            Assert.Contains(ApiPathAnnotations, annotationUrl);
             Assert.Equal(createdAnnotBody, newAnnotation.Body);
 
+        }
+        [Fact]
+        public void Get_All_Annotations_Of_User()
+        {
+            //Login the user in order to get the token
+            UserToken = GetToken();
+
+            var (response, statusCode) = GetObject($"{ApiPathAnnotations}/user", UserToken);
+
+            var noOfPages = (string)response["numberOfPages"];
+            var previousPageUrl = (string)response["prev"];
+            var nextPageUrl = (string)response["next"];
+            var itemsList = (JArray)response["items"];
+
+            Assert.Equal(HttpStatusCode.OK, statusCode);
+            Assert.Null(previousPageUrl);
+            Assert.Null(nextPageUrl);
+            Assert.Equal("1", noOfPages);
+
+            JObject firstItem = (JObject)itemsList[0];
+            var body = (string)firstItem["body"];
+            var postId = (string)firstItem["postId"];
+            var questionId = (string)firstItem["questionId"];
+
+            Assert.Equal("This is annotation 7284 made using unit test.", body);
+            Assert.Equal("7284", postId);
+            Assert.Equal("7284", questionId);
+        }
+
+        [Fact]
+        public void Get_All_Annotations_Of_User_By_PostId()
+        {
+            //Login the user in order to get the token
+            UserToken = GetToken();
+
+            var (response, statusCode) = GetArray($"{ApiPathAnnotations}/post/7284", UserToken);
+            Assert.Equal(HttpStatusCode.OK, statusCode);
+
+            JObject firstItem = (JObject)response[0];
+            var body = (string)firstItem["body"];
+            Assert.Equal("This is annotation 7284 made using unit test.", body);
+        }
+
+        [Fact]
+        public void Update_Annotation()
+        {
+            AnnotationsDto annotToUpdate = new AnnotationsDto
+            {
+                Body = "This is new annotation body for the 2nd annotation made on post with id 7284"
+            };
+
+            UserToken = GetToken();
+            var newAnnotation = new AnnotationsDto
+            {
+                PostId = 7284,
+                Body = "This is another annotation 7284 made using unit test."
+            };
+
+            var (annotation, statusCodePost) = PostData(ApiPathAnnotations, newAnnotation, UserToken);
+            Assert.Equal(HttpStatusCode.OK, statusCodePost);
+            var annotId = (string)annotation["annotationId"];
+            var statusCodeUpdate = PutData($"{ApiPathAnnotations}/{annotId}", annotToUpdate, UserToken);
+
+            Assert.Equal(HttpStatusCode.NoContent, statusCodeUpdate);
+        }
+
+        [Fact]
+        public void Delete_Annotation_By_Id()
+        {
+            UserToken = GetToken();
+            var newAnnotation = new AnnotationsDto
+            {
+                PostId = 7284,
+                Body = "This is another annotation 7284 made using unit test."
+            };
+
+            var (annotation, statusCodePost) = PostData(ApiPathAnnotations, newAnnotation, UserToken);
+            //var annotId = (string)annotation["annotationId"];
+            var statusCode = DeleteData($"{ApiPathAnnotations}/{annotation["annotationId"]}", UserToken);
+            Assert.Equal(HttpStatusCode.OK, statusCode);
         }
 
         private string GetToken()
@@ -91,21 +171,29 @@ namespace UnitTests.WebServiceUnitTests
 
 
         // Helpers
-        //(JArray, HttpStatusCode) GetArray(string url)
-        //{
-        //    var client = new HttpClient();
-        //    var response = client.GetAsync(url).Result;
-        //    var data = response.Content.ReadAsStringAsync().Result;
-        //    return ((JArray)JsonConvert.DeserializeObject(data), response.StatusCode);
-        //}
+        (JArray, HttpStatusCode) GetArray(string url, string userToken)
+        {
+            var client = new HttpClient();
+            if (!string.IsNullOrEmpty(userToken))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userToken);
+            }
+            var response = client.GetAsync(url).Result;
+            var data = response.Content.ReadAsStringAsync().Result;
+            return ((JArray)JsonConvert.DeserializeObject(data), response.StatusCode);
+        }
 
-        //(JObject, HttpStatusCode) GetObject(string url)
-        //{
-        //    var client = new HttpClient();
-        //    var response = client.GetAsync(url).Result;
-        //    var data = response.Content.ReadAsStringAsync().Result;
-        //    return ((JObject)JsonConvert.DeserializeObject(data), response.StatusCode);
-        //}
+        (JObject, HttpStatusCode) GetObject(string url, string userToken)
+        {
+            var client = new HttpClient();
+            if (!string.IsNullOrEmpty(userToken))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userToken);
+            }
+            var response = client.GetAsync(url).Result;
+            var data = response.Content.ReadAsStringAsync().Result;
+            return ((JObject)JsonConvert.DeserializeObject(data), response.StatusCode);
+        }
 
         (JObject, HttpStatusCode) PostData(string url, object content, string userToken)
         {
@@ -123,24 +211,32 @@ namespace UnitTests.WebServiceUnitTests
             return ((JObject)JsonConvert.DeserializeObject(data), response.StatusCode);
         }
 
-        //HttpStatusCode PutData(string url, object content)
-        //{
-        //    var client = new HttpClient();
-        //    var response = client.PutAsync(
-        //        url,
-        //        new StringContent(
-        //            JsonConvert.SerializeObject(content),
-        //            Encoding.UTF8,
-        //            "application/json")).Result;
-        //    return response.StatusCode;
-        //}
+        HttpStatusCode PutData(string url, object content, string userToken)
+        {
+            var client = new HttpClient();
+            if (!string.IsNullOrEmpty(userToken))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userToken);
+            }
+            var response = client.PutAsync(
+                url,
+                new StringContent(
+                    JsonConvert.SerializeObject(content),
+                    Encoding.UTF8,
+                    "application/json")).Result;
+            return response.StatusCode;
+        }
 
-        //HttpStatusCode DeleteData(string url)
-        //{
-        //    var client = new HttpClient();
-        //    var response = client.DeleteAsync(url).Result;
-        //    return response.StatusCode;
-        //}
+        HttpStatusCode DeleteData(string url, string userToken)
+        {
+            var client = new HttpClient();
+            if (!string.IsNullOrEmpty(userToken))
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", userToken);
+            }
+            var response = client.DeleteAsync(url).Result;
+            return response.StatusCode;
+        }
 
     }
 }
