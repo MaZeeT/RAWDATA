621bec4ae798bcecaa3491269b252ee1dc1e5a7a
Monica Toader <toadermonica@gmail.com>
2019-12-06 13:54:07 +0100

    added hardcoded base url path to authentication fetch

--- WebService/wwwroot/js/services/AuthenticationService.js
+++ WebService/wwwroot/js/services/AuthenticationService.js
@@ -1,7 +1,10 @@
 ﻿define(["jquery"], function () {
+
+    const baseUrl="http://localhost:5001/"
+
     var getLoginUser = async function (incomingUserCredentials, callback) {
         try {
-            const response = await fetch("api/auth/tokens", {
+            const response = await fetch(baseUrl + "api/auth/tokens", {
                 method: 'POST', // or 'PUT'
                 body: JSON.stringify(incomingUserCredentials), // data can be `string` or {object}!
                 headers: {
@@ -14,6 +17,25 @@
             });
             callback(response);
             
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+    var signUpUser = async function (incomingUserCredentials, callback) {
+        try {
+            const response = await fetch(baseUrl + "api/auth/users", {
+                method: 'POST', // or 'PUT'
+                body: JSON.stringify(incomingUserCredentials), // data can be `string` or {object}!
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(function (response) {
+                return response.json();
+            }).then(function (responseBody) {
+                return responseBody;
+            });
+            callback(response);
+
         } catch (error) {
             console.error('Error:', error);
         }
