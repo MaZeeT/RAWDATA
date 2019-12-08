e634919df0a44522fa6d2e09c42c14a801369b43
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2019-12-08 22:42:20 +0100

    some trial and error

--- WebService/wwwroot/js/components/HistoryPage/HistoryComponent.js
+++ WebService/wwwroot/js/components/HistoryPage/HistoryComponent.js
@@ -3,22 +3,33 @@ define(["knockout", "historyService"], function (ko, ds) {
     return function () {
         var token = window.localStorage.getItem('userToken');
 
-        var page = ko.observable(2);
+        var page = 2;
         var maxPages = ko.observable(10);
-
+        var totalPages = ko.observable();
+        var prevUrl = ko.observable();
+        var nextUrl = ko.observable();
+        console.log("maxpage value is: " + maxPages());  //todo remove
+        
         var prevPage = function(){
             if (page > 1) page--;
             console.log("page value is: " + page);  //todo remove
+            console.log("maxpage value is: " + maxPages());  //todo remove
         };
 
         var nextPage = function(){
             page++;
             console.log("page value is: " + page);  //todo remove
         };
-
-        var historyItems = ko.observableArray([]);
-        ds.getHistory(token, page, maxPages,function (response) {
+        
+        var historyItems = ko.observableArray();
+        var items = ko.observableArray();
+        console.log("page value is: " + page);
+        ds.getHistory(token, page, maxPages, function (response) {
             historyItems(response);
+            totalPages(response.numberOfPages);
+            prevUrl(response.prev);
+            nextUrl(response.next);
+            items(response.items);
         });
 
         var deletions = function () {
@@ -26,15 +37,15 @@ define(["knockout", "historyService"], function (ko, ds) {
                 return response;
             })
         };
-
+        
         return {
             maxPages,
             prevPage,
             nextPage,
-            historyItems,
+            items,
             deletions
         };
-
+        
 
     };
 
