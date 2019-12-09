8c6d04ac135c85d140532c0a38b756ca41dbb572
Monica Toader <toadermonica@gmail.com>
2019-12-09 19:20:06 +0100

    added pagination - in progress- save work

--- WebService/wwwroot/js/components/HomePage/HomePage.js
+++ WebService/wwwroot/js/components/HomePage/HomePage.js
@@ -2,20 +2,43 @@
 
     return function () {
 
+        let pageSizeSelection = ko.observableArray(['5', '10', '20', '30', '40', '50']); //selection of pagesizes
+        let getPageSize = ko.observable(5);
+        let currentPage = ko.observable(1);
+
         const placeholderStr = "Search with terms here..."
         let searchTerms = ko.observable(placeholderStr);
+        let searchstring = ko.observable("");
+
+
+        let selectedPageSize = ko.observable();
 
         let searchResult = ko.observableArray([]);
         let showTable = ko.observable(false);
         let totalResults = ko.observable("0");
+
+        
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+
 
         searchTerms.subscribe(function (searchStr) {
             if (searchStr.length === 0) {
                 searchResult([]);
                 return;
             }
+
+            searchstring(searchStr);
+
+            console.log("I am in search ", searchStr);
+            console.log("I am in givenPageSize ", getPageSize());
+            console.log("I am in givenPageNumber ", currentPage());
+            const searchString = "?s=" + searchStr;
+            const searchType = "&stype=0";
+            const pageSize = "&pageSize=" + getPageSize();
+            const pageNumber = "&page=" + currentPage();
             
-            homeserv.getSearchItems(searchStr, function (responseData) {
+            homeserv.getSearchItems(searchString, searchType, pageSize, pageNumber, function (responseData) {
                 if (responseData) {
                     console.log("Responsedata from homeage is: ", responseData);
                     totalResults(responseData.totalResults);
@@ -27,6 +50,88 @@
             });
         });
 
+        selectedPageSize.subscribe(function (value) {
+            console.log("My val is an array ", value[0]);
+            getPageSize(value[0]);
+            let searchStr = searchstring();
+            if (searchStr) {
+
+                console.log("I am in search ", searchStr);
+                console.log("I am in givenPageSize ", getPageSize());
+                console.log("I am in givenPageNumber ", currentPage());
+                const searchString = "?s=" + searchStr;
+                const searchType = "&stype=0";
+                const pageSize = "&pageSize=" + getPageSize();
+                const pageNumber = "&page=" + currentPage();
+
+                homeserv.getSearchItems(searchString, searchType, pageSize, pageNumber, function (responseData) {
+                    if (responseData) {
+                        console.log("Responsedata from homeage is: ", responseData);
+                        totalResults(responseData.totalResults);
+                        searchResult(responseData.items);
+                        console.log(searchResult());
+                        showTable(true);
+                    }
+
+                });
+
+            }
+        });
+        let next = function () {
+            console.log("currentPage page on next", currentPage());
+            currentPage(currentPage() + 1);
+            console.log("currentPage page oafter change next", currentPage());
+
+            let searchStr = searchstring();
+            if (searchStr) {
+
+                console.log("I am in search ", searchStr);
+                console.log("I am in givenPageSize ", getPageSize());
+                console.log("I am in givenPageNumber ", currentPage());
+                const searchString = "?s=" + searchStr;
+                const searchType = "&stype=0";
+                const pageSize = "&pageSize=" + getPageSize();
+                const pageNumber = "&page=" + currentPage();
+
+                homeserv.getSearchItems(searchString, searchType, pageSize, pageNumber, function (responseData) {
+                    if (responseData) {
+                        console.log("Responsedata from homeage is: ", responseData);
+                        totalResults(responseData.totalResults);
+                        searchResult(responseData.items);
+                        console.log(searchResult());
+                        showTable(true);
+                    }
+
+                });
+
+            }
+        }
+        let prev = function () {
+            console.log("currentPage page on prev", currentPage());
+            const pageValueUpdated = currentPage();
+            if (pageValueUpdated > 1) {
+                currentPage(pageValueUpdated - 1);
+            }
+            console.log("currentPage page after change prev", currentPage());
+        }
+
+
+        let callService = function (searchString, searchType, pageSize, pageNumber) {
+
+            return homeserv.getSearchItems(searchString, searchType, pageSize, pageNumber, function (responseData) {
+                if (responseData) {
+                    console.log("Responsedata from homeage is: ", responseData);
+                    totalResults(responseData.totalResults);
+                    searchResult(responseData.items);
+                    console.log(searchResult());
+                    showTable(true);
+                }
+
+            });
+        }
+
+       
+
         let selectSearchResultItem = function (item) {
             console.log("Item.threadlink is: ", item.threadLink);
             console.log("Item is: ", item);
@@ -36,18 +141,22 @@
         };
 
 
-
-
-
-
-
-
         return {
             searchTerms,
             searchResult,
             showTable,
             totalResults,
-            selectSearchResultItem
+            selectSearchResultItem,
+            //getPg,
+            //pageSizeSelection,
+            //getpgsize,
+            //pgsizechanged,
+            currentPage,
+            next,
+            prev,
+            pageSizeSelection,
+            getPageSize,
+            selectedPageSize
 
         }
     }
--- WebService/wwwroot/js/services/HomePageService.js
+++ WebService/wwwroot/js/services/HomePageService.js
@@ -2,58 +2,36 @@
 
 
 
+    var getSearchItems = async function (searchString, searchType, pageSize, pageNumber, callback) {
         const baseUrl = "http://localhost:5001/";
-        const path ="api/search/"
+        const path = "api/search/"
 
-        var urlApiCall = function (baseUrl, path, searchStr) {
-            //const searchString = "?s=" + searchStr;
-            //const searchType = "&stype=" + sType;
-            //const pageSize = "&pageSize=" + pSize;
-            //const pageNumber = "&page=" + pNum;
+        const searchUrl = baseUrl + path + searchString + searchType + pageNumber + pageSize;
+        console.log("URL: ", searchUrl);
+        try {
+            const response = await fetch(searchUrl, {
+                method: 'GET', // or 'PUT'
+                // body: JSON.stringify(incomingUserCredentials), // data can be `string` or {object}!
 
-            const searchString = "?s=" + searchStr;
-            const searchType = "&stype=0";
-            const pageSize = "&pageSize=5";
-            const pageNumber = "&page=10";
-            const url = baseUrl + path + searchString + searchType + pageNumber + pageSize;
-            return url;
+                headers: new Headers({
+                    'Authorization': 'Bearer ' + window.localStorage.getItem("userToken"),
+                    'Content-Type': 'application/json'
+                }),
+            }).then(function (response) {
+                return response.json();
+            }).then(function (responseBody) {
+                return responseBody;
+            });
+            callback(response);
+
+        } catch (error) {
+            console.error('Error:', error);
         }
+    };
 
-        var getSearchItems = async function (searchStr, callback) {
-            const searchString = "?s=" + searchStr;
-            const searchType = "&stype=0";
-            const pageSize = "&pageSize=5";
-            const pageNumber = "&page=10";
-            const searchUrl = baseUrl + path + searchString + searchType + pageNumber + pageSize;
-            
-            console.log("Search Url: ", searchUrl);
-            try {
-                const response = await fetch(searchUrl, {
-                    method: 'GET', // or 'PUT'
-                   // body: JSON.stringify(incomingUserCredentials), // data can be `string` or {object}!
-
-                    headers: new Headers({
-                        'Authorization': 'Bearer ' + window.localStorage.getItem("userToken"),
-                        'Content-Type': 'application/json'
-                    }), 
-                }).then(function (response) {
-                    return response.json();
-                }).then(function (responseBody) {
-                    return responseBody;
-                });
-                callback(response);
-
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        };
-
-
-
-
-        return {
-            getSearchItems
-        }
+    return {
+        getSearchItems
+    }
 
 
 });
