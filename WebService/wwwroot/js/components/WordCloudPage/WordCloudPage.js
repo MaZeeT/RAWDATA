297bdb8a6199434ee2ff170bbabcf8fad7ba5458
Pornflake <satanika9@gmail.com>
2019-12-10 16:11:17 +0100

    wordcloud stylins wip

--- WebService/wwwroot/js/components/WordCloudPage/WordCloudPage.js
+++ WebService/wwwroot/js/components/WordCloudPage/WordCloudPage.js
@@ -9,12 +9,14 @@
         let max = 15;
         let stype = 4;
 
+        let stypebtn = ko.observable("tfidf");
+
         let selectedValue = ko.observable(15);
 
      //   let width = 200;
      //   let height = 200;
 
-        const placeholderStr = "Search with terms here..."
+        const placeholderStr = "Input search terms here..."
         let searchTerms = ko.observable(placeholderStr);
 
         let searchResult = ko.observableArray([]);
@@ -49,27 +51,30 @@
         };*/
 
         let clrsearchfield = function upd() {
-            console.log("searchreerm : ", searchTerms());
+            //console.log("searchreerm : ", searchTerms());
             if (searchTerms() === placeholderStr) {
                 searchTerms('');
             }
-            console.log("searchreerm : ", searchTerms());
+            //console.log("searchreerm : ", searchTerms());
         }
 
         let cloudupdate = function upd() {
 
-            console.log("searchreerm : ", searchTerms());
-            console.log("stypw : ", stype);
+            //console.log("searchreerm : ", searchTerms());
+            //console.log("stypw : ", stype);
             max = selectedValue();
-            console.log("maxres : ", max);
-            console.log("slider value : ", selectedValue());
+            //console.log("maxres : ", max);
+            //console.log("slider value : ", selectedValue());
+            //console.log("stypeval : ", stypebtn());
+
+            if (stypebtn() == 'tfidf') { stype = 4; } else stype = 5;
 
             wc.getWCItems(searchTerms(), stype, max, function (data) {
-                console.log("Data from api call search : ", data);
+                //console.log("Data from api call search : ", data);
 
                 if (data) {
 
-                    console.log("data status : ", data.status);
+                    //console.log("data status : ", data.status);
 
                     if (data.status == 400) {
                         //bad request
@@ -88,7 +93,7 @@
                         data1 = data.map(function (a) {
                             return { text: a.term, weight: a.rank };
                         });
-                        console.log("datamap: ", data1);
+                        //console.log("datamap: ", data1);
 
                         $('#cloud').jQCloud('destroy');
                         $('#cloud').jQCloud(data1,
@@ -107,11 +112,12 @@
                 return;
             };
             max = selectedValue();
-            console.log("maxres : ", max);
-            console.log("slider value : ", selectedValue());
+            //console.log("maxres : ", max);
+            //console.log("slider value : ", selectedValue());
+            if (stypebtn() == 'tfidf') { stype = 4; } else stype = 5;
 
             wc.getWCItems(searchStr, stype, max, function (data) {
-                console.log("Data from api call search : ", data);
+                //console.log("Data from api call search : ", data);
                 searchTerms(searchStr);
                 if (data) {
                     loaded(true);
@@ -120,7 +126,7 @@
                     data1 = data.map(function (a) {
                         return { text: a.term, weight: a.rank };
                     });
-                    console.log("datamap: ", data1);
+                    //console.log("datamap: ", data1);
 
                     $('#cloud').jQCloud('destroy'); /// cant figure out how to update lol! so am destroying it..
                     $('#cloud').jQCloud(data1,
@@ -153,6 +159,7 @@
            // enter,
            // noofresultschanged,
             cloudupdate,
+            stypebtn,
             clrsearchfield,
             selectedValue,
             loaded
