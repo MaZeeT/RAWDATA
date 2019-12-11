d7c5fda646db0904c108e749263a389e0470a50b
Pornflake <satanika9@gmail.com>
2019-12-11 14:56:45 +0100

    updat

--- WebService/wwwroot/js/utils/util.js
+++ WebService/wwwroot/js/utils/util.js
@@ -2,27 +2,19 @@
 
     //return named querystring value
     function getParameterByName(name, url) {
-        //if (!url) url = window.location.href; //if no url was passed, try to get the current one, we dont use this in this app
-        //console.log("namfore: ", name);
-        //name = name.replace(/[\[\]]/g, '\\$&');
-        //console.log("namfore: ", name);
 
         //example url: http://localhost:5001/api/search?s=gnu,bear,gcc&stype=0&page=1&pageSize=5
         //example name: page
 
         var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'), //this line builds the regex string
-        //regex now contain the following /[?&]page(=([^&#]*)|&|#|$)/
+        //regex now contains the following /[?&]page(=([^&#]*)|&|#|$)/ - this matches from the ? or & character before 'page' to the & # $ after
             results = regex.exec(url);
-        //regex.exec() return an array of matches
+        //regex.exec() returns an array of matches
         //results now contain ["?page=2", "=2", "2"]
-        console.log("regex: ", regex);
-        console.log("resul: ", results);
-        console.log("resul0: ", results[0]);
-        console.log("resul1: ", results[1]);
-        console.log("resul2: ", results[2]);
+
         if (!results) return null; //if nothing matched at all
         if (!results[2]) return ''; //if there was no value (only '=')
-        return decodeURIComponent(results[2]); //return special characters (if any) to original form (like %20 - ' ')
+        return decodeURIComponent(results[2]); //transform special characters (if any) to original form (like %20 becomes ' ') and return the queryvalue we have now located
     };
 
 
