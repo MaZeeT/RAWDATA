5cca089c541053c6e2c86f942c406954cd8ea969
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2023-01-12 23:37:59 +0100

    Upgrade to minimal hosting model

--- WebService/Program.cs
+++ WebService/Program.cs
@@ -1,17 +1,55 @@
-using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Text;
+using DatabaseService;
+using DatabaseService.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
-namespace WebService
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddSingleton<ISharedService, SharedService>();
+builder.Services.AddSingleton<ISearchDataService, SearchDataService>();
+builder.Services.AddSingleton<IAnnotationService, AnnotationService>();
+builder.Services.AddSingleton<IAppUserService, AppUserService>();
+builder.Services.AddSingleton<IHistoryService, HistoryService>();
+builder.Services.AddSingleton<ISearchHistoryService, SearchHistoryService>();
+
+var key = Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Auth:Key").Value);
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
-    public class Program
+    options.TokenValidationParameters = new TokenValidationParameters
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateLifetime = true,
+        ValidateAudience = false,
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+//Needed for serving wwwroot files frontend
+app.UseFileServer();
+
+//Needed for api routing
+app.UseRouting();
+
+//Authentication & authorization
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+app.Run();
