339eaa1a85b6e93840ad6c8563b61dcd3a07dc83
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2025-06-03 23:08:20 +0200

    Moved dependency registration to another class

---
+++ WebService/ConfigureServices.cs
@@ -0,0 +1,41 @@
+﻿using DomainServices.Implementations;
+using DomainServices.Interfaces;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Repositories.Implementation;
+using Repositories.Interfaces;
+
+namespace WebService;
+
+public static class ServiceConfigurator
+{
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        const string database = "host=localhost;port=5432;db=stackoverflow;uid=postgres;pwd=Password123";
+        
+        services.AddControllers();
+
+        services.AddSingleton<IAnnotationService, AnnotationService>();
+        services.AddSingleton<IBookmarkService, BookmarkService>();
+        services.AddSingleton<IHistoryService, HistoryService>();
+        services.AddSingleton<IThreadService, ThreadService>();
+        services.AddSingleton<ISearchService, SearchService>();
+        services.AddSingleton<IUserService, UserService>();
+
+        services.AddSingleton<IAnnotationRepository, AnnotationRepository>();
+        services.AddSingleton<IHistoryRepository, HistoryRepository>();
+        services.AddSingleton<IQuestionRepository, QuestionRepository>();
+        services.AddSingleton<ISearchRepository, SearchDataRepository>();
+        services.AddSingleton<IUserRepository, AppUserRepository>();
+        services.AddSingleton<ISearchHistoryRepository, SearchHistoryRepository>();
+        services.AddSingleton<ISharedRepository, SharedRepository>();
+
+        services.AddPooledDbContextFactory<DatabaseContext2>(options =>
+        {
+            options
+                .UseLoggerFactory(DatabaseContext2.MyLoggerFactory)
+                .UseNpgsql(database);
+        });
+    }
+}
--- WebService/Program.cs
+++ WebService/Program.cs
@@ -1,46 +1,18 @@
 using System;
 using System.Text;
-using DomainServices.Implementations;
-using DomainServices.Interfaces;
-using Infrastructure.Database;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using Repositories.Implementation;
-using Repositories.Interfaces;
+using WebService;
 
-const string database = "host=localhost;port=5432;db=stackoverflow;uid=postgres;pwd=Password123";
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
-builder.Services.AddSingleton<IAnnotationService, AnnotationService>();
-builder.Services.AddSingleton<IBookmarkService, BookmarkService>();
-builder.Services.AddSingleton<IHistoryService, HistoryService>();
-builder.Services.AddSingleton<IThreadService, ThreadService>();
-builder.Services.AddSingleton<ISearchService, SearchService>();
-builder.Services.AddSingleton<IUserService, UserService>();
-
-builder.Services.AddSingleton<IAnnotationRepository, AnnotationRepository>();
-builder.Services.AddSingleton<IHistoryRepository, HistoryRepository>();
-builder.Services.AddSingleton<IQuestionRepository, QuestionRepository>();
-builder.Services.AddSingleton<ISearchRepository, SearchDataRepository>();
-builder.Services.AddSingleton<IUserRepository, AppUserRepository>();
-builder.Services.AddSingleton<ISearchHistoryRepository, SearchHistoryRepository>();
-builder.Services.AddSingleton<ISharedRepository, SharedRepository>();
-
-builder.Services.AddPooledDbContextFactory<DatabaseContext2>(options =>
-{
-    options
-        .UseLoggerFactory(DatabaseContext2.MyLoggerFactory)
-        .UseNpgsql(database);
-});
+ServiceConfigurator.ConfigureServices(builder.Services);
 
 var key = Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Auth:Key").Value);
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
     options.TokenValidationParameters = new TokenValidationParameters
     {
