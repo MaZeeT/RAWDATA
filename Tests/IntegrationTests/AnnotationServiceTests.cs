67dfb84b613bb5c29fff88c607a4735e922efad8
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2025-06-03 23:09:04 +0200

    Added test for AnnotationService

---
+++ Tests/IntegrationTests/AnnotationServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using Domain.AnnotationsDTOs;
+using DomainServices.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using WebService;
+
+namespace Tests.IntegrationTests;
+
+public class AnnotationServiceTests
+{
+    private IAnnotationService _sut;
+    private const int UserId = 12;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var services = new ServiceCollection();
+        ServiceConfigurator.ConfigureServices(services);
+        var serviceProvider = services.BuildServiceProvider();
+        _sut = serviceProvider.GetRequiredService<IAnnotationService>();
+        
+    }
+    
+    [Test]
+    public void AddAnnotation_Successfully()
+    {
+        var annotation = new AnnotationsDto
+        {
+            UserId = UserId,
+            PostId = 14,
+            Body = "AddAnnotation_Successfully test"
+        };
+        
+        _sut.CreateAnnotation(annotation,  out var result);
+        
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Is.GreaterThan(0));
+    }
+}
