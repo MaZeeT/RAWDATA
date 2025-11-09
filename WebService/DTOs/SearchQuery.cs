4216ea59ae866d2e8f1ffc1da815749b64d8aadf
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2025-11-09 22:18:25 +0100

    Fixed mapping between parameter naming.

--- WebService/DTOs/SearchQuery.cs
+++ WebService/DTOs/SearchQuery.cs
@@ -1,7 +1,12 @@
-﻿namespace WebService.DTOs;
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebService.DTOs;
 
 public class SearchQuery
 {
+    [FromQuery(Name = "s")] //Todo remove this mapping from SearchTerm to s when frontend is being worked on
     public string SearchTerms { get; set; } // comma-delimited
+    
+    [FromQuery(Name = "stype")] //Todo remove this mapping from SearchType to stype when frontend is being worked on
     public int SearchType { get; set; } = 3; // this sets stype to 3 if there is no stype param
 }
