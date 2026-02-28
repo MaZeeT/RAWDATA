5622708086f611d2b872ff8e6a74df362591cb45
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2026-02-28 23:35:29 +0100

    Added Annotations to Bruno

--- Domain/AnnotationsDTOs/AnnotationsDto.cs
+++ Domain/AnnotationsDTOs/AnnotationsDto.cs
@@ -2,14 +2,14 @@
 {
     public class AnnotationsDto
     {
-        public string URL { get; set; }
+        public string? URL { get; set; }
         public int AnnotationId { get; set; } // hide from user
         public int UserId { get; set; }  // hide from user
         public int HistoryId { get; set; }  // hide from user
         public int PostId { get; set; }
         public string Body { get; set; }
         public DateTime Date { get; set; } 
-        public string AddAnnotationUrl { get; set; }
+        public string? AddAnnotationUrl { get; set; }
 
         public static AnnotationsDto MapFrom(Annotations annotations)
         {
