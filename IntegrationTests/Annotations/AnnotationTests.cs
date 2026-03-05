08afe8775d9e09345d3d9ec0bccc075fa16e9dc0
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2026-03-05 21:53:58 +0100

    Replaced hardcoded token with a call to fetch new valid token in constructor

--- IntegrationTests/Annotations/AnnotationTests.cs
+++ IntegrationTests/Annotations/AnnotationTests.cs
@@ -1,5 +1,9 @@
 ﻿using System.Net;
 using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using Domain.AnnotationsDTOs;
 using Microsoft.AspNetCore.Mvc.Testing;
 using WebService.Controllers;
 using Xunit;
@@ -8,14 +12,56 @@ namespace IntegrationTests.Annotations;
 
 public class AnnotationTests : IClassFixture<WebApplicationFactory<AnnotationsController>>
 {
-    private const string Token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjMwNSIsIm5iZiI6MTc3MjMxNjc1NywiZXhwIjoxNzcyNDAzMTU3LCJpYXQiOjE3NzIzMTY3NTd9.UNSZDvmVcPZLJ3Rrq7-45S-BTaEPmWR2cEVTSV3P6vs";
     private readonly HttpClient _httpClient;
-
+    
     public AnnotationTests(WebApplicationFactory<AnnotationsController> factory)
     {
         _httpClient = factory.CreateDefaultClient();
-        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+        var token = GetToken().Result;
+        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    }
+
+    private async Task<string> GetToken()
+    {
+        var response = await _httpClient.PostAsJsonAsync(
+            "/api/auth/tokens",  
+            new { username = "mazeettest", password = "testtest" }, 
+            TestContext.Current.CancellationToken
+            );
+        using var doc = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+        var token = doc.RootElement.GetProperty("token").GetString() ?? string.Empty;
+        return token;
+    }
+
+    
+    [Fact]
+    public async Task Get_ReturnOk_ForExistingAnnotations()
+    {
+        // Arrange
+        var annotationOne = new AnnotationsDto()
+        {
+            PostId = 19,
+            Body = "First test annotation",
+        };
+        
+        var annotationTwo = new AnnotationsDto()
+        {
+            PostId = 19,
+            Body = "Second test annotation",
+        };
+        
+        var json = "{\n  \"postId\" : 1,\n  \"body\": \"test text\"\n}";
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        
+        var responseOne = await _httpClient.PostAsync("/api/annotations", content, TestContext.Current.CancellationToken);
+            
+        
+        // Act
+        var response = await _httpClient.GetAsync("/api/annotations/422", TestContext.Current.CancellationToken);
+        
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        
     }
     
     [Fact]
