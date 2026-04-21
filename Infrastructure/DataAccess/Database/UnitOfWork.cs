999edb08d0714dc41c61fe2eb46ad1aca3ea486f
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2026-04-21 23:27:13 +0200

    Added Unit of work

---
+++ Infrastructure/DataAccess/Database/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Application.Interfaces.Repositories;
+
+namespace Infrastructure.DataAccess.Database;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DatabaseContext _context;
+
+    public UnitOfWork(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+}
--- Web/ConfigureServices.cs
+++ Web/ConfigureServices.cs
@@ -1,5 +1,4 @@
-﻿using Application.Interfaces;
-using Application.Interfaces.Repositories;
+﻿using Application.Interfaces.Repositories;
 using Application.Interfaces.Services;
 using Application.Services;
 using Infrastructure.DataAccess.Database;
@@ -17,21 +16,23 @@ public static class ServiceConfigurator
         
         services.AddControllers();
 
-        services.AddSingleton<IAnnotationService, AnnotationService>();
-        services.AddSingleton<IBookmarkService, BookmarkService>();
-        services.AddSingleton<IHistoryService, HistoryService>();
-        services.AddSingleton<IThreadService, ThreadService>();
-        services.AddSingleton<ISearchService, SearchService>();
-        services.AddSingleton<IUserService, UserService>();
+        services.AddScoped<IAnnotationService, AnnotationService>();
+        services.AddScoped<IBookmarkService, BookmarkService>();
+        services.AddScoped<IHistoryService, HistoryService>();
+        services.AddScoped<IThreadService, ThreadService>();
+        services.AddScoped<ISearchService, SearchService>();
+        services.AddScoped<IUserService, UserService>();
 
-        services.AddSingleton<IAnnotationRepository, AnnotationRepository>();
-        services.AddSingleton<IHistoryRepository, HistoryRepository>();
-        services.AddSingleton<IQuestionRepository, QuestionRepository>();
-        services.AddSingleton<ISearchRepository, SearchDataRepository>();
-        services.AddSingleton<IUserRepository, AppUserRepository>();
-        services.AddSingleton<ISearchHistoryRepository, SearchHistoryRepository>();
-        services.AddSingleton<ISharedRepository, SharedRepository>();
+        services.AddScoped<IAnnotationRepository, AnnotationRepository>();
+        services.AddScoped<IHistoryRepository, HistoryRepository>();
+        services.AddScoped<IQuestionRepository, QuestionRepository>();
+        services.AddScoped<ISearchRepository, SearchDataRepository>();
+        services.AddScoped<IUserRepository, AppUserRepository>();
+        services.AddScoped<ISearchHistoryRepository, SearchHistoryRepository>();
+        services.AddScoped<ISharedRepository, SharedRepository>();
 
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
         services.AddDbContextFactory<DatabaseContext>(options =>
         {
             options
