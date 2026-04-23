543211f26ca1973431571ea139b8ae0721626042
MaZeeT <30165307+MaZeeT@users.noreply.github.com>
2026-04-23 22:25:51 +0200

    Moved old test into new structure

--- UnitTests/ServiceTests/AnnotationServiceTests.cs
+++ UnitTests/ServiceTests/AnnotationServiceTests.cs
@@ -9,6 +9,7 @@ namespace UnitTests.ServiceTests;
 public class AnnotationServiceTests : IClassFixture<ServiceProviderFixture>
 {
     private readonly IAnnotationService _sut;
+    private const int TestUserId = 12;
     
     public AnnotationServiceTests(ServiceProviderFixture fixture)
     {
@@ -22,5 +23,21 @@ public class AnnotationServiceTests : IClassFixture<ServiceProviderFixture>
         _sut.CreateAnnotation(s, out var newId);
         
         Assert.Equal(-1, newId);
+    }
+    
+    [Fact]
+    public void AddAnnotation_Successfully()
+    {
+        var annotation = new AnnotationsDto
+        {
+            UserId = TestUserId,
+            PostId = 19,
+            Body = "AddAnnotation_Successfully test",
+            Date = new DateTime(2025, 11, 11, 11,11,11,11,DateTimeKind.Utc)
+        };
+        
+        _sut.CreateAnnotation(annotation,  out var result);
+        
+        Assert.True(result > 0);
     }
 }
